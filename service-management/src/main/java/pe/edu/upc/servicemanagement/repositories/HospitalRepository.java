bb74b616bc2fd211e86e2355f19ff61cba0f1428
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-26 17:35:22 -0500

    eliminacion

--- service-management/src/main/java/pe/edu/upc/servicemanagement/repositories/HospitalRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.servicemanagement.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.servicemanagement.entities.Hospital;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-
-}
--- service-management/src/main/java/pe/edu/upc/servicemanagement/services/HospitalService.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.servicemanagement.services;
-
-
-import pe.edu.upc.servicemanagement.entities.Hospital;
-
-public interface HospitalService extends CrudService<Hospital, Integer> {
-
-}
