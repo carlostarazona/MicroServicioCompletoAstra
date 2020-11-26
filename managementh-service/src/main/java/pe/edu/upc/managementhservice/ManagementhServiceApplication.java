76b4de66215cd82ad021ac31c304e87ecb4b0d29
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-26 17:45:34 -0500

    update

--- managementh-service/src/main/java/pe/edu/upc/managementhservice/ManagementhServiceApplication.java
+++ managementh-service/src/main/java/pe/edu/upc/managementhservice/ManagementhServiceApplication.java
@@ -3,8 +3,7 @@ package pe.edu.upc.managementhservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,10 +13,6 @@ public class ManagementhServiceApplication {
 		SpringApplication.run(ManagementhServiceApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
-	}
 
 
 }
