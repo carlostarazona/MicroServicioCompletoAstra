73fde6c7d4219f98cc34bdcd999bc99ee90f4300
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-26 16:50:26 -0500

    eliminacion temporal de micro servicio

--- service-management/src/main/java/pe/edu/upc/servicemanagement/repositories/HospitalRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.servicemanagement.repositories;
-
-import pe.edu.upc.servicemanagement.entities.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-
-}
