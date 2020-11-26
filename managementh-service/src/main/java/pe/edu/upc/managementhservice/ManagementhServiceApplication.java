a031b551139d7b890b5280451398a5b4638a2366
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-26 17:40:39 -0500

    ultimo commit

--- managementh-service/src/main/java/pe/edu/upc/managementhservice/ManagementhServiceApplication.java
+++ managementh-service/src/main/java/pe/edu/upc/managementhservice/ManagementhServiceApplication.java
@@ -2,12 +2,22 @@ package pe.edu.upc.managementhservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ManagementhServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementhServiceApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
 
 }
