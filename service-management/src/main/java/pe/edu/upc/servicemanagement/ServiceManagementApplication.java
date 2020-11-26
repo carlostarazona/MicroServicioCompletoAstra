df5968c4ac38fcb597424170df57cc04abd5a305
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-26 17:04:18 -0500

    actualizaciones

--- service-management/src/main/java/pe/edu/upc/servicemanagement/ServiceManagementApplication.java
+++ service-management/src/main/java/pe/edu/upc/servicemanagement/ServiceManagementApplication.java
@@ -2,12 +2,17 @@ package pe.edu.upc.servicemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ServiceManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceManagementApplication.class, args);
+	}
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 
 }
